41f6f501080ba7eb1b409cb17d56b071981967a1
Matt C <matt.cheah@gmail.com>
2017-02-04 00:58:03 -0800

    style more, add template files, create admin section

---
+++ admin/login.php
@@ -0,0 +1,23 @@
+<!doctype html>
+
+<html>
+
+<head>
+    <title>Login</title>
+    <meta name="robots" content="noindex">
+</head>
+<body>
+    
+    <form submit="form-login.php">
+    
+        <label for="un">Username:</label>
+        <input type="text" name="un" placeholder="Username:">
+        <label for="pw">Password:</label>
+        <input type="text" name="pw" placeholder="Password:">
+        <input type="submit" name="submit" value="Log On">
+    
+    </form>
+
+</body>
+
+</html>
---
+++ template-files/footer.php
@@ -0,0 +1,16 @@
+<footer>
+   <div class="row">
+    
+        <div class="col-md-3 col-sm-6">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Page1</a></li>
+                <li><a href="">Page2</a></li>
+            </ul>
+        </div>
+        <div class="col-md-3 col-sm-6">
+       
+        </div>
+       
+    </div>     
+</footer>
